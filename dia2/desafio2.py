b7b90fd3dd17a69c6179b91195145dad0a2e9415
anakin <amb.test19@gmail.com>
2024-04-28 21:25:15 -0300

    initial commit

---
+++ dia2/desafio2.py
@@ -0,0 +1,7 @@
+# programa en python para calcular comisiones 
+nombre = input("Por favor, dime tu nombre: ")
+ventas = int(input("Diga sus ventas totales del mes: "))
+
+comision = round(ventas * 13 / 100,2)
+
+print(f"Hola {nombre}, tus comisiones de este mes son de ${comision}")
